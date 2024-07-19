47c6ca910aa3aaf4abf5e2d9e51703c24d21a24c
Jo Yaconelli <jo@shades.news>
2024-07-18 18:34:22 -0700

    fix type

--- src/components/useMousePosition.tsx
+++ src/components/useMousePosition.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = React.useState({
+  const [mousePosition, setMousePosition] = React.useState<{
+    x: number | null;
+    y: number | null;
+  }>({
     x: null,
     y: null,
   });
   React.useEffect(() => {
-    const updateMousePosition = (ev) => {
+    const updateMousePosition = (ev: MouseEvent) => {
       setMousePosition({ x: ev.clientX, y: ev.clientY });
     };
     window.addEventListener("mousemove", updateMousePosition);
